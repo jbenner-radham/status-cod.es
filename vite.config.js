73166afd69553cb4ccefd8ff869e2572a1ef6b07
James Benner <james.benner@gmail.com>
2022-11-16 16:03:48 -0500

    Hardcoded dcterms:available

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,6 @@ import dateTime from '@radioactivehamster/date-time';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
-const buildTime = dateTime();
-
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
@@ -13,8 +11,7 @@ export default defineConfig({
         createHtmlPlugin({
             inject: {
                 data: {
-                    available: buildTime, // Hardcode this value after initial deploy
-                    modified: buildTime
+                    modified: dateTime()
                 }
             }
         })
